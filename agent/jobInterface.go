ee1a94bd893ccf0e0a29f375af638b535667a3f0
燕涛 <15243650315@163.com>
2019-05-17 16:18:24 +0800

    [update] 调整job子系统接口

--- agent/jobInterface.go
+++ agent/jobInterface.go
@@ -4,7 +4,9 @@ package agent
 type IJob interface {
 
 	// job模块运行入口
-	Run(param interface{})
-
-	Return()	// job模块运行完成结果出口
+	// jobId: job id
+	// param: job模块需要的相关参数
+	// resultChan: 结果发送通道，job模块运行完成后将结果发送至该通道
+	Run(jobID string, param map[string]interface{}, resultChan chan map[string]interface{})
+	Return(resultChan chan map[string]interface{})	// job模块运行完成结果出口
 }
