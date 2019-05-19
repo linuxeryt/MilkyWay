747f1cbe03fc1d84c94964cc7905980218419e79
燕涛 <15243650315@163.com>
2019-05-19 22:21:22 +0800

    [update] 增加命令行HostID和etcd集群列表配置

--- agent/cmd/main.go
+++ agent/cmd/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"context"
+	"flag"
+	"log"
 	"milkyway/agent/job"
 	"sync"
 )
 
+
+var EtcdEndPoints = flag.String("etcdEndpoints", "", "etcd endpoints")
+var HostID = flag.String("hostid", "", "agent唯一标识，使用外网IP")
+
+
+func init() {
+	flag.Parse()
+
+	if *EtcdEndPoints  == "" {
+		log.Fatal("Need EtcdEndPoints param")
+		flag.Usage()
+	}
+
+	if *HostID == "" {
+		// 使用hostname 作为HostID
+	}
+}
 
 func main() {
 	var wg sync.WaitGroup
